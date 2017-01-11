d39eb2c5fdc875d9916d496dd0a8477534ea1742
Tihh Gonçalves <tihh.gon@gmail.com>
2017-01-11 11:15:57 -0200

    up

--- pages/financeiro-transferencias.form.php
+++ pages/financeiro-transferencias.form.php
@@ -5,6 +5,13 @@ template_getHeader();
 
 $form = new girafaFORM('Financeiro - Transferências', 'financeiro-transferencias_action.php', 'FinanceiroTransferencias', 'Valor');
 
+$tipo = false;
+if(!empty(@$form->reg->DeConta)){
+    $tipo = 'CON';
+} elseif(!empty($form->reg->DeContaInterna)){
+    $tipo = 'INT';
+}
+
 /* DADOS  */
 $box = new girafaFORM_box('Informações');
 
@@ -14,12 +21,23 @@ $html  = '<label class="col-sm-2 control-label">Data</label>';
 $html .= '<div class="col-sm-3">' . form_field_date('Data', @$form->reg->Data, 100) .'</div>';
 $box->AddContent($html);
 
-//$box->AddContentBreakLine();
+$box->AddContentBreakLine();
+
+//Nome
+$html  = '<label class="col-sm-2 control-label">Tipo de Transferência</label>';
+$options = array(
+    'CON' => 'Contas',
+    'INT' => 'Contas Internas'
+);
+$html .= '<div class="col-sm-3">' . form_field_list('TIPO', $options, '') .'</div>';
+$box->AddContent($html);
+
 $box->AddContentLine();
-$box->AddContent('<h4>Contas de Origem</h4>');
+
+$box->AddContent('<div id="contas">');
 
 //Origem - Conta
-$html  = '<label class="col-sm-2 control-label">Conta</label>';
+$html  = '<label class="col-sm-2 control-label">Da Conta</label>';
 $options = array();
 
 $sql  = 'SELECT * FROM FinanceiroContas';
@@ -28,15 +46,31 @@ $sql .= " ORDER BY Nome ASC";
 $contas = $db->LoadObjects($sql);
 
 foreach($contas as $conta){
-    $options[$conta->ID] = $conta->Nome;
+    $options[$conta->ID] = $conta->Nome . ' (saldo: R$' . number_format(financeiroContaSaldo($conta->ID), 2, ',', '.') . ')';
 }
 
 $html .= '<div class="col-sm-3">' . form_field_list('DeConta', $options, @$form->reg->DeConta, null, false) .'</div>';
 $box->AddContent($html);
 
 
+//Destino - Conta
+$html  = '<label class="col-sm-2 control-label">Para Conta</label>';
+$options = array();
+
+foreach($contas as $conta){
+    $options[$conta->ID] = $conta->Nome . ' (saldo: R$' . number_format(financeiroContaSaldo($conta->ID), 2, ',', '.') . ')';
+}
+
+$html .= '<div class="col-sm-3">' . form_field_list('ParaConta', $options, @$form->reg->ParaConta, null, false) .'</div>';
+$box->AddContent($html);
+
+
+$box->AddContent('</div>');
+
+$box->AddContent('<div id="contasinternas">');
+
 //Origem - Conta Interna
-$html  = '<label class="col-sm-2 control-label">Conta Interna</label>';
+$html  = '<label class="col-sm-2 control-label">Da Conta Interna</label>';
 $options = array();
 
 $sql  = 'SELECT * FROM FinanceiroContasInternas';
@@ -45,40 +79,26 @@ $sql .= " ORDER BY Nome ASC";
 $contasInternas = $db->LoadObjects($sql);
 
 foreach($contasInternas as $conta){
-    $options[$conta->ID] = $conta->Nome;
+    $options[$conta->ID] = $conta->Nome . ' (saldo: R$' . number_format(financeiroContaInternaSaldo($conta->ID), 2, ',', '.') . ')';
 }
 
 $html .= '<div class="col-sm-3">' . form_field_list('DeContaInterna', $options, @$form->reg->DeContaInterna, null, false) .'</div>';
 $box->AddContent($html);
 
 
-//$box->AddContentBreakLine();
-$box->AddContentLine();
-$box->AddContent('<h4>Contas de Destino</h4>');
-
-//Origem - Conta
-$html  = '<label class="col-sm-2 control-label">Conta</label>';
-$options = array();
-
-foreach($contas as $conta){
-    $options[$conta->ID] = $conta->Nome;
-}
-
-$html .= '<div class="col-sm-3">' . form_field_list('ParaConta', $options, @$form->reg->ParaConta, null, false) .'</div>';
-$box->AddContent($html);
-
-
-//Origem - Conta Interna
-$html  = '<label class="col-sm-2 control-label">Conta Interna</label>';
+//Destino - Conta Interna
+$html  = '<label class="col-sm-2 control-label">Para Conta Interna</label>';
 $options = array();
 
 
 foreach($contasInternas as $conta){
-    $options[$conta->ID] = $conta->Nome;
+    $options[$conta->ID] = $conta->Nome . ' (saldo: R$' . number_format(financeiroContaInternaSaldo($conta->ID), 2, ',', '.') . ')';
 }
 
 $html .= '<div class="col-sm-3">' . form_field_list('ParaContaInterna', $options, @$form->reg->ParaContaInterna, null, false) .'</div>';
 $box->AddContent($html);
+
+$box->AddContent('</div>');
 
 $box->AddContentLine();
 
@@ -94,3 +114,55 @@ $form->PrintHTML();
 
 template_getFooter();
 ?>
+<script>
+
+    function selecionaTipo(val){
+        if(val == 'INT') {
+            $('#contasinternas')
+                                    .show()
+                                    .find('select').attr('required', 'required')
+                                    .parent().addClass('field_list_required');
+
+            $('#contas select')
+                                    .val('').trigger('chosen:updated')
+                                    .removeAttr('required');
+        } else if(val == 'CON') {
+            $('#contas')
+                                    .show()
+                                    .find('select').attr('required', 'required')
+                                    .parent().addClass('field_list_required');
+
+            $('#contasinternas select')
+                                    .val('').trigger('chosen:updated')
+                                    .removeAttr('required');
+        } else {
+            $('#contasinternas, #contas').hide();
+        }
+    }
+
+    $(document).ready(function(){
+
+        $('#contasinternas, #contas').hide();
+
+        $('select[name=TIPO]').change(function(){
+
+            $('#contasinternas, #contas').hide();
+
+            var val = $(this).val();
+            selecionaTipo(val);
+
+
+        });
+
+
+        <?
+        if($tipo != false){
+        ?>
+        $('select[name=TIPO]').val('<?= $tipo; ?>').trigger("chosen:updated");
+        selecionaTipo('<?= $tipo; ?>');
+        <?
+        }
+        ?>
+    });
+
+</script>
--- pages/financeiro-transferencias.grid.php
+++ pages/financeiro-transferencias.grid.php
@@ -3,28 +3,63 @@ template_getHeader();
 
 $grid = new girafaGRID('FinanceiroTransferencias', 'Financeiro - Transferências');
 
+$grid->AddFilter('Entre Contas', 'DeConta IS NOT NULL');
+$grid->AddFilter('Entre Contas Internas', 'DeContaInterna IS NOT NULL');
+
 $field_data = new girafaGRID_field('Data');
 $field_data->isDate();
 
-$field_deConta = new girafaGRID_field('DeConta', 'Da Conta');
-$field_deConta->isTable('FinanceiroContas', 'Nome');
+$field_tipo = new girafaGRID_field('TIPO', 'Tipo de Transferência');
+$field_tipo->isCustom();
 
-$field_deContaInterna = new girafaGRID_field('DeContaInterna', 'Da Conta Interna');
-$field_deContaInterna->isTable('FinanceiroContasInternas', 'Nome');
-
-$field_paraConta = new girafaGRID_field('ParaConta', 'Para Conta');
-$field_paraConta->isTable('FinanceiroContas', 'Nome');
-
-$field_paraContaInterna = new girafaGRID_field('ParaContaInterna', 'Para Conta Interna');
-$field_paraContaInterna->isTable('FinanceiroContasInternas', 'Nome');
+$field_contas = new girafaGRID_field('CONTAS', 'Contas');
+$field_contas->isCustom();
 
 $field_valor = new girafaGRID_field('Valor', 'Valor');
 $field_valor->isMoney();
 
 
-$grid->addFields(array($field_data, $field_deConta, $field_paraConta, $field_deContaInterna, $field_paraContaInterna, $field_valor));
+$grid->addFields(array($field_data, $field_tipo, $field_contas, $field_valor));
 
 $grid->PrintHTML();
 
 template_getFooter();
+
+
+function macro_grid_before($field, $reg){
+
+  if($field == 'TIPO'){
+
+    if(!empty($reg->DeConta))
+      return 'Entre Contas';
+    elseif(!empty($reg->DeContaInterna))
+      return 'Entre Contas Internas';
+  }
+
+
+  if($field == 'CONTAS'){
+
+    if(!empty($reg->DeConta)) {
+      $contaA = $reg->DeConta;
+      $contaAtable = LoadRecord('FinanceiroContas', $contaA);
+      $contaAnome = $contaAtable->Nome;
+      $contaB = $reg->ParaConta;
+      $contaBtable = LoadRecord('FinanceiroContas', $contaB);
+      $contaBnome = $contaBtable->Nome;
+
+      return $contaAnome . ' <i class="fa fa-long-arrow-right" aria-hidden="true"></i> ' . $contaBnome;
+
+    } elseif(!empty($reg->DeContaInterna)){
+      $contaA = $reg->DeContaInterna;
+      $contaAtable = LoadRecord('FinanceiroContasInternas', $contaA);
+      $contaAnome = $contaAtable->Nome;
+      $contaB = $reg->ParaContaInterna;
+      $contaBtable = LoadRecord('FinanceiroContasInternas', $contaB);
+      $contaBnome = $contaBtable->Nome;
+
+      return $contaAnome . ' <i class="fa fa-long-arrow-right" aria-hidden="true"></i> ' . $contaBnome;
+    }
+  }
+
+}
 ?>
